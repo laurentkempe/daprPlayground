090694d27f0bd8cf86403485ec58dbd01fe6582e
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2021-10-09 11:50:59 +0200

    Add Http job

--- CronHttpBindings/Worker/Controllers/ScheduledHttpJobController.cs
+++ CronHttpBindings/Worker/Controllers/ScheduledHttpJobController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Worker.Controllers
@@ -8,16 +10,22 @@ namespace Worker.Controllers
     public class ScheduledHttpJobController : ControllerBase
     {
         private readonly ILogger<ScheduledHttpJobController> _logger;
+        private readonly DaprClient _daprClient;
 
-        public ScheduledHttpJobController(ILogger<ScheduledHttpJobController> logger)
+        public ScheduledHttpJobController(ILogger<ScheduledHttpJobController> logger, DaprClient daprClient)
         {
             _logger = logger;
+            _daprClient = daprClient;
         }
 
         [HttpPost]
-        public void HttpJob()
+        public async Task HttpJob()
         {
             _logger.LogInformation($"{nameof(ScheduledHttpJobController)} called 😎");
+
+            var response = await _daprClient.InvokeBindingAsync<string, TimeData>("httpJob", "get", "");
+
+            _logger.LogInformation($"Received: {response.utc_datetime}");
         }
     }
 }
