1237e82b761db9047a905e03be31978c1c130e6b
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2022-10-30 10:59:53 +0100

    Add state.proto and have project compile

--- FileSystemStateStorePluggableComponent/Program.cs
+++ FileSystemStateStorePluggableComponent/Program.cs
@@ -11,7 +11,9 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGrpcService<FileSystemStateService>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ FileSystemStateStorePluggableComponent/Services/FileSystemStateService.cs
@@ -0,0 +1,13 @@
+﻿using Dapr.Proto.Components.V1;
+
+namespace FileSystemStateStorePluggableComponent.Services;
+
+public sealed class FileSystemStateService : StateStore.StateStoreBase
+{
+    private readonly ILogger<FileSystemStateService> _logger;
+
+    public FileSystemStateService(ILogger<FileSystemStateService> logger)
+    {
+        _logger = logger;
+    }
+}
